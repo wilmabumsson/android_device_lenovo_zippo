2b172925b30a61f5a272ddd522eafc06a332f88d
Einar Gednochsson <pete80pro.pb@gmail.com>
2021-04-06 13:35:03 +0200

    zippo: Adapt for Derpfest

--- derp_zippo.mk
+++ derp_zippo.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The TenX Project
+# Copyright (C) 2021 The Derpfest Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,19 +7,21 @@
 $(call inherit-product, device/lenovo/zippo/device.mk)
 
 # Inherit some common Derpfest stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+DERP_BUILDTYPE := Official
 EXTRA_FOD_ANIMATIONS := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_INCLUDE_STOCK_ARCORE := true
-CUSTOM_BUILD_TYPE := Official
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Lenovo
 PRODUCT_DEVICE := zippo
 PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo Z6 Pro
-PRODUCT_NAME := aosp_zippo
+PRODUCT_NAME := derp_zippo
 
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"
 
